cb795d2c15533d44ff4a953ced86046f2e537aad
Miley Wright <dishaliekwright@yahoo.com>
2020-09-21 15:54:22 -0400

    small changes

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -6,9 +6,12 @@ def earliest_ancestor(ancestors, starting_node):
     relationships = {}
 
     for ancestor in ancestors:
-        if ancestor[1] not in relationships:
-            relationships[ancestor[1]] = set()
-        relationships[ancestor[1]].add(ancestor[0])
+        parent = ancestor[0]
+        child = ancestor[1]
+
+        if child not in relationships:
+            relationships[child] = set()
+        relationships[child].add(parent)
 
     if starting_node in relationships:
         queue.enqueue(relationships[current_node])
